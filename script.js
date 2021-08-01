fe82844d318b661040a6096c21d68dd06854bf86
thelscon <thelscon@gmail.com>
2021-08-01 22:56:21 +0300

    v2

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict' ;
 
+const thisScript = document.body.lastElementChild ;
+
 const h1 = document.createElement ( 'h1' ) ;
 h1.textContent = 'Игорь Довганюк' ;
+thisScript.before ( h1 ) ;
 
 const p = document.createElement ( 'p' ) ;
 p.textContent = 'Задача: создать страницу с элементами <h1>, <p>, <input> и <button> без использования html,'
                 + 'а лишь используя методы объекта window.document. В заголовке <h1> должны быть ваше имя и фамилия,'
                 + 'в <p> - текст задания, которое вы сейчас выполняете. В <input> можно что-то ввести,'
                 + 'если нажать на кнопку то будет выведен alert c введенным в поле значением и после закрытия alert\'а поле будет очищаться.' ;
+thisScript.before ( p ) ;
 
 const input = document.createElement ( 'input' ) ;
 input.type = 'text' ;
+thisScript.before ( input ) ;
 
 const button = document.createElement ( 'button' ) ;
 button.textContent = 'Кнопка Alert' ;
@@ -23,6 +28,4 @@ button.addEventListener ( 'click' , () => {
         alert ( 'Ничего не введено' ) ;
     }
 } )
-
-document.body.prepend ( h1 , p , input , button ) ;
-
+thisScript.before ( button ) ;
